ae88e0d2050499630e34a9f0f4e6780d4f3a2c0f
Olivier_Luethy <olivier@kauz.ch>
2022-06-27 11:09:13 +0200

    Better background-image for background

--- script.js
+++ script.js
@@ -15,23 +15,19 @@ window.addEventListener('keyup', (event) => {
     switch (event.key) {
         case 'ArrowLeft':
             rotation -= 10;
-            myCircle.style.left = parseInt(myCircle.style.left) - 5 + 'px';
             myCircle.style.transform = 'rotate(' + rotation + 'deg)';
             console.log(rotation + " deg");
             break;
         case 'ArrowRight':
             rotation += 10;
-            myCircle.style.left = parseInt(myCircle.style.left) + 5 + 'px';
             myCircle.style.transform = 'rotate(' + rotation + 'deg)';
             console.log(rotation + " deg");
             break;
         case 'ArrowUp':
-            myCircle.style.top = parseInt(myCircle.style.top) - 5 + 'px';
             myCircle.style.transform = 'rotate(' + rotation + 'deg)';
             console.log(rotation + " deg");
             break;
         case 'ArrowDown':
-            myCircle.style.top = parseInt(myCircle.style.top) + 5 + 'px';
             myCircle.style.transform = 'rotate(' + rotation + 'deg)';
             console.log(rotation + " deg");
             break;
